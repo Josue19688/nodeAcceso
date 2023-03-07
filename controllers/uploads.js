984fcf9ed577c58f9067b5ef6b2187ad8837a1ac
avasquel13@miumg.edu.gt <advin.vasquez@CORTE.LOCAL>
2023-03-07 17:23:03 -0600

    modificando controlador uploads

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -91,7 +91,7 @@ const fileUpload = ( req, res = response ) => {
     const tipo = req.params.tipo;
     const id   = req.params.id;
 
-    // Validar tipo
+   
     const tiposValidos = ['novedades','archivo','usuario','visitas'];
     if ( !tiposValidos.includes(tipo) ){
         return res.status(400).json({
@@ -100,7 +100,7 @@ const fileUpload = ( req, res = response ) => {
         });
     }
 
-    // Validar que exista un archivo
+    
     if (!req.files || Object.keys(req.files).length === 0) {
         return res.status(400).json({
             ok: false,
@@ -108,13 +108,13 @@ const fileUpload = ( req, res = response ) => {
         });
     }
 
-    // Procesar la imagen...
+   
     const file = req.files.images;
 
-    const nombreCortado = file.name.split('.'); // wolverine.1.3.jpg
+    const nombreCortado = file.name.split('.'); 
     const extensionArchivo = nombreCortado[ nombreCortado.length - 1 ];
     
-    // Validar extension
+    
     const extensionesValidas = ['png','jpg','jpeg','gif','pdf'];
     if ( !extensionesValidas.includes( extensionArchivo ) ) {
         return res.status(400).json({
@@ -123,13 +123,13 @@ const fileUpload = ( req, res = response ) => {
         });
     }
 
-    // Generar el nombre del archivo
+    
     const nombreArchivo = `${ uuidv4() }.${ extensionArchivo }`;
 
-    // Path para guardar la imagen
+    
     const path = `./uploads/${ tipo }/${ nombreArchivo }`;
 
-    // Mover la imagen
+  
     file.mv( path , (err) => {
         if (err){
             console.log(err)
@@ -139,7 +139,7 @@ const fileUpload = ( req, res = response ) => {
             });
         }
 
-        // Actualizar base de datos
+       
         actualizarImagen( tipo, id, nombreArchivo );
 
         res.json({
